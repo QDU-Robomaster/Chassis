37d5361efa435066b0e17d0d77d07fa5162c8af7
llLeo <li13780602203@qq.com>
2025-10-05 03:47:34 +0800

    update

--- Mecanum.hpp
+++ Mecanum.hpp
@@ -9,20 +9,102 @@ required_hardware: []
 depends: []
 === END MANIFEST === */
 // clang-format on
-
+#include "CMD.hpp"
 #include "Chassis.hpp"
+#include "RMMotorContainer.hpp"
 #include "app_framework.hpp"
+#include "message.hpp"
+#include "pid.hpp"
+#include "thread.hpp"
 
+template <typename MotorType>
 class Mecanum {
  public:
-  Mecanum(LibXR::HardwareContainer &hw, LibXR::ApplicationManager &app) {
-    // Hardware initialization example:
-    // auto dev = hw.template Find<LibXR::GPIO>("led");
+  Mecanum(LibXR::HardwareContainer &hw, LibXR::ApplicationManager &app, CMD &cmd,
+       Motor<MotorType> &motor_can1, Motor<MotorType> &motor_can2,
+       uint32_t task_stack_depth, LibXR::PID<float>::Param pid_param)
+      : motor_can1_(&motor_can1),
+        motor_can2_(&motor_can2),
+        cmd_(&cmd),
+        pid_param_(pid_param) {
+    thread_.Create(this, ThreadFunction, "MecanumThread", task_stack_depth,
+                   LibXR::Thread::Priority::HIGH);
   }
 
-  void SelfResolution() {}
+  static void ThreadFunction(Mecanum *mecanum) {
+    LibXR::Topic::ASyncSubscriber<CMD::ChassisCMD> cmd_suber("chassis_cmd");
+
+    cmd_suber.StartWaiting();
+    while (true) {
+      if (cmd_suber.Available()) {
+        mecanum->cmd_data_ = cmd_suber.GetData();
+        cmd_suber.StartWaiting();
+      }
+
+      mecanum->SelfResolution();
+      mecanum->KinematicsInverseResolution();
+      mecanum->SteerMotorKinematicsNearestTransposition();
+      mecanum->OutputToDynamics();
+    }
+  }
+
+  void Update() {}
+
+  void SelfResolution() {
+    now_vx_ = 0.0f;
+    now_vy_ = 0.0f;
+
+    for (int i = 0; i < 4; i++) {
+      now_vx_ += 0.0f;
+      now_vy_ += 0.0f;
+    }
+  }
+
   void KinematicsInverseResolution() {}
+  void SteerMotorKinematicsNearestTransposition() {}
   void OutputToDynamics() {}
 
  private:
+  const float wheel_radius_ = 0.0f;
+
+  const float wheel_to_center_ = 0.0f;
+
+  const float gravity_height_ = 0.0f;
+
+  float target_wheel_omega_[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+  float target_wheel_current_[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+  float static_wheel_current_[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+  float dynamic_wheel_current_[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+  float wheel_resistance_omega_threshold_ = 0.0f;
+
+  float wheel_speed_limit_factor_ = 0.5f;
+
+  float now_vx_ = 0.0f;
+  float now_vy_ = 0.0f;
+
+  float now_omega_ = 0.0f;
+
+  float angle_pitch_ = 0.0f;
+  float angle_roll_ = 0.0f;
+
+  float slope_direction_x_ = 0.0f;
+  float slope_direction_y_ = 0.0f;
+  float slope_direction_z_ = 0.0f;
+
+  float target_vx_ = 0.0f;
+  float target_vy_ = 0.0f;
+
+  float target_omega_ = 0.0f;
+
+  Motor<MotorType> *motor_can1_;
+  Motor<MotorType> *motor_can2_;
+  CMD *cmd_;
+  LibXR::PID<float>::Param pid_param_;
+  LibXR::Thread thread_;
+
+  CMD::ChassisCMD cmd_data_;
 };
